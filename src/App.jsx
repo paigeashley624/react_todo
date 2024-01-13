8622019f15f374fd2763901fca435fc76a976bd9
Paige Morton <paigeashley624@gmail.com>
2024-01-13 13:42:16 -0500

    add todo list template

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import "./styles.css"
 
 export default function App(){
-  return <form className="new-item-form">
+  return(
+  <>
+    <form className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
       <input type="text" id="item" />
     </div>
     <button className="btn">Add</button>
-  </form>
+    </form>
 
-  <h1 className="header"> Todo List</h1>
+    <h1 className="header"> Todo List</h1>
+    <ul className="list">
+      <li>
+        <label type="checkbox">
+          Item 1
+        </label>
+        <button className="btn btn-danger"> Delete</button>
+      </li>
+      <li>
+        <label type="checkbox">
+          Item 2
+        </label>
+        <button className="btn btn-danger"> Delete</button>
+      </li>
+    </ul>
+  </>
+  )
 }
